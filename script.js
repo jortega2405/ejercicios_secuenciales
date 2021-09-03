e0d28605a0715375d27a87069fd2a3b6490b1faf
Jose Ortega <jose.ort98@gmail.com>
2021-09-02 21:47:23 -0500

    ejercicio 5 parte 2

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function y1(){
 };
 console.log(y1());
 
+/*********************************************************/
 function y2(){
     const z = 5;
     const n = 3;
@@ -15,6 +16,7 @@ function y2(){
 };
 console.log(y2());
 
+/*********************************************************/
 function y3(){
     const z = 5;
     const n = ((8+2-4)**2 * 5 + 8 + 7 / 2 - 30* 5 ) / 2 * 5 - 3;
@@ -24,17 +26,20 @@ function y3(){
 };
 console.log(y3());
 
+/*********************************************************/
 function ejercicio1 (presion, volumen, temperatura){
     return (presion * volumen) / (0.37 * (temperatura + 460));
 };
 console.log(ejercicio1(43,38.5,35));
 
+/*********************************************************/
 function ejercicio2 (edad){
     let num_pul = (200 - edad) / 10;
     return `el numero de pulsaciones es de ${num_pul}`;
 };
 console.log(ejercicio2(23));
 
+/*********************************************************/
 function ejercicio3 (inversion1, inversion2, inversion3){
     const total = inversion1 + inversion2 + inversion3;
     porcentaje1 = (inversion1 * 100 / total);
@@ -47,10 +52,30 @@ function ejercicio3 (inversion1, inversion2, inversion3){
 };
 console.log(ejercicio3(500, 900, 700));
 
+/*********************************************************/
+
 function ejercicio4 (saldoInicial) {
     return (saldoInicial* 0.015) + saldoInicial;
 };
 console.log(ejercicio4(500));
+
+/*********************************************************/
+
+function ejercicio5(sueldoBase) {
+    const datos = {
+        ley : sueldoBase* 0.01,
+        seguroSocial: sueldoBase* 0.04,
+        seguroForsozo: sueldoBase* 0.005,
+        caja: sueldoBase* 0.05,
+        totalPagar: sueldoBase - (sueldoBase* 0.01) - (sueldoBase* 0.04) - (sueldoBase* 0.005) - (sueldoBase* 0.05)
+    }
+    return datos;
+};
+console.log(ejercicio5(2500000));
+
+/*********************************************************/
+
+
 
 
 
