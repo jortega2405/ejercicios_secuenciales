3abefa524f0f440f0d887399bfb2551be1e3d421
Jose Ortega <jose.ort98@gmail.com>
2021-09-02 21:15:28 -0500

    Ejercicio 3 y3

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ function y2(){
     return y;
 };
 console.log(y2());
+
+function y3(){
+    const z = 5;
+    const n = ((8+2-4)**2 * 5 + 8 + 7 / 2 - 30* 5 ) / 2 * 5 - 3;
+    const m = z**2 * 3 + n;
+    const y = ((((z+2-n)**2 * m+8/2 - 30) / 2 * 5 - 3)**5 + 15 * 3 - 9 / 3)**2 - 5 / 4;
+    return y;
+};
+console.log(y3());
