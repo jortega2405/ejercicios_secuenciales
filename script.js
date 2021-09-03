bab407262690eedd1a48f667723e05e086138933
Jose Ortega <jose.ort98@gmail.com>
2021-09-02 21:02:50 -0500

    ejercicio 2 y2

--- script.js
+++ script.js
@@ -6,15 +6,11 @@ function y1(){
 };
 console.log(y1());
 
-function getMasa(presion, volumen, temperatura){
-    let a = 0.37;
-    let b = 460;
-    return masa = ((presion * volumen) / (a * (temperatura + b)));
-}
-console.log(getMasa(40, 54.5, 17));
-
-function pulsaciones(edad) {
-    let num_pul = ( 200 - edad)/ 10;
-    return `el numero de pulsaciones es de ${num_pul}`;
-}
-console.log(pulsaciones(17));
+function y2(){
+    const z = 5;
+    const n = 3;
+    const m = z - n;
+    const y = (((z + 2 - n)**2 * m + 8 / 2 - 30) / 2 * 5 - 3 )**5 + 15 * 3 - 9 / 3;
+    return y;
+};
+console.log(y2());
